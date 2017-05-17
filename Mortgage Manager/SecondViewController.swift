24a7d62e079ff30c91c9d58d035398d034461d7a
siddhisuthar <siddhi@siddhis-macbook-pro.local>
2017-05-17 15:43:28 -0700

    Initial Commit

---
+++ Mortgage Manager/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mortgage Manager
+//
+//  Created by Siddhi Suthar on 5/17/17.
+//  Copyright © 2017 Siddhi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
