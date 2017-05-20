4ca7eb1f4450d504bb4c59ea4d69bb7ac9b5bd91
siddhisuthar <siddhi@siddhis-macbook-pro.local>
2017-05-20 02:31:01 -0700

    calculation view updates

--- Mortgage Manager/FirstViewController.swift
+++ Mortgage Manager/FirstViewController.swift
@@ -7,14 +7,154 @@
 //
 
 import UIKit
+import DropDown
+import CoreLocation
+import GoogleMaps
+import Firebase
+import FirebaseDatabase
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var propertyType: UIButton!
+    let selectPropertyType = DropDown()
+    @IBOutlet weak var street: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var selectState: UIButton!
+    @IBOutlet weak var zipcode: UITextField!
+    let selectStateDropdown = DropDown()
+    @IBOutlet weak var housePrice: UITextField!
+    @IBOutlet weak var annualInterestRate: UITextField!
+    @IBOutlet weak var downPaymentAmount: UITextField!
+    @IBOutlet weak var monthlyPayment: UILabel!
+    @IBOutlet weak var mortgageLoanLength: UIButton!
+    let selectLoanLength = DropDown()
+   
+    var coordinate = CLLocationCoordinate2D()
+    var geocoder = CLGeocoder()
+   
+    
+    @IBAction func selectPropertyType(_ sender: Any) {
+        selectPropertyType.show()
+    }
+    @IBAction func selectMortgageLoanLength(_ sender: Any) {
+        selectLoanLength.show()
+    }
+    @IBAction func selectStateClick(_ sender: Any) {
+        selectStateDropdown.show()
+    }
+    
+    @IBAction func calculatePayment(_ sender: Any) {
+
+        let lengthOfMortgageLoan = Int(mortgageLoanLength.titleLabel!.text!)
+        
+        if ((housePrice.text?.characters.count)!>0 && (downPaymentAmount.text?.characters.count)!>0 && (annualInterestRate.text?.characters.count)!>0 && lengthOfMortgageLoan! != 0) {
+           
+            let monthlyIntRate: Double = Double(annualInterestRate.text!)! / (12 * 100);
+            let months: Double = Double(lengthOfMortgageLoan! * 12);
+            
+            let loanAmount = Double(housePrice.text!)! - Double(downPaymentAmount.text!)!;
+            
+            let monthlyPaymentAmount: Double = (loanAmount * monthlyIntRate) / (1 - pow((1+monthlyIntRate), -months))
+            
+            monthlyPayment.text = String(format: "%.2f", monthlyPaymentAmount)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupStateDropDown()
+        setupLoanLengthDropDown()
+        setupPropertyTypeDropDown()
     }
 
+    @IBAction func didTapSave(_ sender: Any) {
+        if (street.text?.characters.count)!>0 && (city.text?.characters.count)!>0 && (zipcode.text?.characters.count)!>0 {
+            
+            let address = "\(street.text!), \(city.text!),  \(selectState.titleLabel!.text!) \(zipcode.text!)"
+            print("address is: \(address)")
+            
+            geocoder.geocodeAddressString(address) { (placemarks, error) in
+                // Process Response
+                self.processResponse(withPlacemarks: placemarks, error: error)
+            }
+        }
+        postToDatabase()
+    }
+     func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+        
+        if let error = error {
+            print("Unable to Forward Geocode Address (\(error))")
+            locationLabel.text = "Unable to Find Location for Address"
+            
+        } else {
+            var location: CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+            
+            if let location = location {
+                coordinate = location.coordinate
+                locationLabel.text = "\(coordinate.latitude) \(coordinate.longitude)"
+            } else {
+                locationLabel.text = "No Matching Location Found"
+            }
+        }
+    }
+    
+    //setting up dropdowns
+    func setupStateDropDown(){
+    
+        selectStateDropdown.anchorView = selectState
+        selectStateDropdown.direction = .bottom
+        selectStateDropdown.dataSource = ["CA", "AZ", "TX"]
+        selectStateDropdown.bottomOffset = CGPoint(x: 0, y: selectState.bounds.height)
+        
+        selectStateDropdown.selectionAction = { [unowned self] (index, item) in
+            self.selectState.setTitle(item, for: .normal)
+        }
+    }
+    func setupLoanLengthDropDown(){
+        selectLoanLength.anchorView = mortgageLoanLength
+        selectLoanLength.direction = .bottom
+        selectLoanLength.dataSource = ["15", "30"]
+        selectLoanLength.bottomOffset = CGPoint(x: 0, y: mortgageLoanLength.bounds.height)
+        
+        selectLoanLength.selectionAction = { [unowned self] (index, item) in
+            self.mortgageLoanLength.setTitle(item, for: .normal)
+        }
+    }
+    func setupPropertyTypeDropDown(){
+        selectPropertyType.anchorView = propertyType
+        selectPropertyType.direction = .bottom
+        selectPropertyType.dataSource = ["House", "Townhouse", "Condo"]
+        selectPropertyType.bottomOffset = CGPoint(x: 0, y: propertyType.bounds.height)
+        
+        selectPropertyType.selectionAction = { [unowned self] (index, item) in
+            self.propertyType.setTitle(item, for: .normal)
+        }
+    }
+    
+    func postToDatabase(){
+       
+        let calculation: NSDictionary = [
+         "streetAddr" : street.text!,
+         "cityAddr" : city.text!,
+         "stateAddr" : selectState.titleLabel!.text!,
+         "zip" : zipcode.text!,
+         "hPrice" : housePrice.text!,
+         "anr" : annualInterestRate.text!,
+         "dPayment" : downPaymentAmount.text!,
+         "loanlength" : mortgageLoanLength.titleLabel!.text!,
+         "mAmount" : monthlyPayment.text! ]
+        
+       let databaseRef = FIRDatabase.database().reference()
+        databaseRef.child("calculations").childByAutoId().setValue(calculation)
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Mortgage Manager/SecondViewController.swift
+++ Mortgage Manager/SecondViewController.swift
@@ -7,13 +7,54 @@
 //
 
 import UIKit
+import GoogleMaps
+import Firebase
+import FirebaseDatabase
 
 class SecondViewController: UIViewController {
 
+    var coordinatePassed = CLLocationCoordinate2D()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadDatafromDatabase()
+     
+        let f: CGRect = self.view.frame
+        let mapFrame: CGRect  = CGRect.init(x: f.origin.x, y: 50, width: f.size.width, height: f.size.height)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: mapFrame, camera: camera)
+     
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+       
+        self.view.addSubview(mapView)
     }
+    
+    func loadDatafromDatabase(){
+        
+        let currentRef = FIRDatabase.database().reference().child("calculations")
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        var tempItems = [NSDictionary]()
+        currentRef.observe(.value, with: { snapshot in
+            
+            for item in snapshot.children {
+                let child = item as! FIRDataSnapshot
+                let dict = child.value as! NSDictionary
+                tempItems.append(dict)
+            }
+            print("temp items: ")
+            print(tempItems)
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        })
+       // forward geocoding
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
